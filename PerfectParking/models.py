580b759b9a870fdffe6505bf46b433b7cb82403e
RhysQuilter <98286839+Rhys-LIT@users.noreply.github.com>
2023-02-01 20:42:48 +0000

    model update

--- PerfectParking/models.py
+++ PerfectParking/models.py
@@ -7,8 +7,8 @@ class ParkingLot(models.Model):
     address = models.CharField(max_length=255)
     hours = models.CharField(max_length=255)
     isPaidParking = models.BooleanField(default=True)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=17, decimal_places=15)
+    longitude = models.DecimalField(max_digits=17, decimal_places=15)
     image = models.ImageField(upload_to='images/', blank=True)
     
     def __str__(self):
@@ -18,8 +18,8 @@ class ParkingLotMonitor(models.Model):
     id = models.AutoField(primary_key=True)
     parkingLot = models.ForeignKey(ParkingLot, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, unique=True)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=17, decimal_places=15)
+    longitude = models.DecimalField(max_digits=17, decimal_places=15)
     ProbabilityParkingAvailable = models.DecimalField(max_digits=5, decimal_places=2)
     dateTimeLastUpdated = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=True)
