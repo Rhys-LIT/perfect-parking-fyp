83ab0abfd0ae753d8f116aaae0112d1e4173b8bd
RhysQuilter <98286839+Rhys-LIT@users.noreply.github.com>
2023-02-01 20:30:35 +0000

    view1 one Parking Lot

--- PerfectParking/urls.py
+++ PerfectParking/urls.py
@@ -10,4 +10,5 @@ class WebPaths:
 urlpatterns = [
     path(WebPaths.ROOT, views.index, name='index'),
     path(WebPaths.PARKING_LOTS, views.parking_lots, name='parking-lots'),
+    path(f'{WebPaths.PARKING_LOTS}/<int:parking_lot_id>', views.parking_lot, name='parking-lot')
 ]
--- PerfectParking/views.py
+++ PerfectParking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 from .models import ParkingLot
@@ -6,7 +6,9 @@ from .models import ParkingLot
 
 class WebPages:
     HOME_PAGE = 'website/index.html'
+    PARKING_LOT = 'website/parking-lot.html'
     PARKING_LOTS = 'website/parking-lots.html'
+    
 
 
 def index(request):
@@ -16,3 +18,8 @@ def index(request):
 def parking_lots(request):
     parking_lots: list = ParkingLot.objects.all()
     return render(request, WebPages.PARKING_LOTS, {'parking_lots': parking_lots})
+
+def parking_lot(request, parking_lot_id):
+    parking_lot = get_object_or_404(ParkingLot, pk=parking_lot_id)
+    return render(request, WebPages.PARKING_LOT, {'parking_lot': parking_lot})
+
