02fe45ea2042a0108f9dc1f01b9f5335dc353e8c
RhysQuilter <98286839+Rhys-LIT@users.noreply.github.com>
2023-02-01 20:18:06 +0000

    Admin

--- PerfectParking/admin.py
+++ PerfectParking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from PerfectParking.models import ParkingLot, ParkingLotMonitor
+
 # Register your models here.
+
+admin.site.register(ParkingLot)
+admin.site.register(ParkingLotMonitor)
