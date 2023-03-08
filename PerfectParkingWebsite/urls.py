1ca4a15880a361002dc93b780e8e4128e340b60b
RhysQuilter <98286839+Rhys-LIT@users.noreply.github.com>
2023-03-08 13:13:01 +0000

    serve uploaded images

--- PerfectParkingWebsite/urls.py
+++ PerfectParkingWebsite/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -30,3 +32,4 @@ urlpatterns = [
     path('api-auth/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
