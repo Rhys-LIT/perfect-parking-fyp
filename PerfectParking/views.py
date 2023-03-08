9d19e7b4571ddd15fe372277814ceb2db6b67414
RhysQuilter <98286839+Rhys-LIT@users.noreply.github.com>
2023-03-08 13:33:20 +0000

    little fixes

--- PerfectParking/views.py
+++ PerfectParking/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import ParkingLot
 
@@ -8,7 +8,6 @@ class WebPages:
     HOME_PAGE = 'website/index.html'
     PARKING_LOT = 'website/parking-lot.html'
     PARKING_LOTS = 'website/parking-lots.html'
-    
 
 
 def index(request):
@@ -19,7 +18,7 @@ def parking_lots(request):
     parking_lots: list = ParkingLot.objects.all()
     return render(request, WebPages.PARKING_LOTS, {'parking_lots': parking_lots})
 
+
 def parking_lot(request, parking_lot_id):
     parking_lot = get_object_or_404(ParkingLot, pk=parking_lot_id)
     return render(request, WebPages.PARKING_LOT, {'parking_lot': parking_lot})
-
--- PerfectParkingWebsite/urls.py
+++ PerfectParkingWebsite/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     path('', include('PerfectParking.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
