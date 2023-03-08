9ac3da0c8603c4e059a5591672c9ad9f5f01442f
RhysQuilter <98286839+Rhys-LIT@users.noreply.github.com>
2023-03-08 14:41:46 +0000

    starting register

--- PerfectParking/views.py
+++ PerfectParking/views.py
@@ -8,6 +8,7 @@ class WebPages:
     HOME_PAGE = 'website/index.html'
     PARKING_LOT = 'website/parking-lot.html'
     PARKING_LOTS = 'website/parking-lots.html'
+    REGISTER_USER = 'website/register.html'
 
 
 def index(request):
@@ -15,6 +16,14 @@ def index(request):
 
 
 def parking_lots(request):
+    """Builds the parking lots page. 
+
+    Args:
+        request (HttpRequest): _description_
+
+    Returns:
+        _type_: _description_
+    """
     parking_lots: list = ParkingLot.objects.all()
     return render(request, WebPages.PARKING_LOTS, {'parking_lots': parking_lots})
 
@@ -22,3 +31,19 @@ def parking_lots(request):
 def parking_lot(request, parking_lot_id):
     parking_lot = get_object_or_404(ParkingLot, pk=parking_lot_id)
     return render(request, WebPages.PARKING_LOT, {'parking_lot': parking_lot})
+
+def register_user(request):
+    """Guest User registers to use the app
+
+    Args:
+        request (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    
+    # load the user details from the request
+    # create a new user
+    
+   
+    return render(request, WebPages.REGISTER_USER)
