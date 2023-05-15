21f3e364b894d7fa54df73cd4a56eb4d064b6790
Rhys-LIT <98286839+Rhys-LIT@users.noreply.github.com>
2023-05-15 18:05:56 +0100

    test fix

--- PerfectParkingClient/tests/parking_lot_tests.py
+++ PerfectParkingClient/tests/parking_lot_tests.py
@@ -3,7 +3,7 @@ import sys
 import unittest
 
 from requests import request
-from perfectparking import ParkingMonitorData, RestApiUtility
+from PerfectParkingClient.perfectparking import ParkingMonitorData, RestApiUtility
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -12,16 +12,16 @@ class RestApiUtilityTestSuite(unittest.TestCase):
     """Rest Api Utility test cases."""
 
 
-def test_update_server_parking_monitor_data(self):
-    """Test update_server_parking_monitor_data method."""
-    parking_monitor_data = ParkingMonitorData()
-    free_spaces_in_frame: float = 3
-    probability_parking_available: 0.5  # (3/6)
-    response: request.Response = RestApiUtility.update_server_parking_monitor_data(
-        parking_monitor_data, free_spaces_in_frame, probability_parking_available
-    )
-    expected_response_status_code = 200
-    self.assertEqual(response.status_code, expected_response_status_code)
+    def test_update_server_parking_monitor_data(self):
+        """Test update_server_parking_monitor_data method."""
+        parking_monitor_data:ParkingMonitorData = ParkingMonitorData()
+        free_spaces_in_frame: float = 3
+        probability_parking_available:float = 0.5  # (3/6)
+        response: request.Response = RestApiUtility.update_server_parking_monitor_data(
+            parking_monitor_data, free_spaces_in_frame, probability_parking_available
+        )
+        expected_response_status_code = 200
+        self.assertEqual(response.status_code, expected_response_status_code)
 
 
 if __name__ == "__main__":
